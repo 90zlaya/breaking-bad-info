9abdc11d71d81c773bc41320c483fec49b5f453b
90zlaya <contact@zlatanstajic.com>
2019-09-08 23:06:29 +0200

    Header and footer added

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,44 @@
 <template>
   <div>
-    <h1>Hello World from {{ title }}</h1>
+    <app-header></app-header>
+    <div class="app-content">
+      <h1>Hello World from {{ title }}</h1>
+      <h2 v-if="subtitle">{{ subtitle }}</h2>
+      <button v-if="!subtitle" v-on:click="addSubtitle">Add subtitle</button>
+    </div>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 
 export default {
 
   /* ------------------------------------------------------------------------ */
 
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+  },
+
+  /* ------------------------------------------------------------------------ */
+
   data () {
     return {
-      title: "Vue.js Playground"
+      title: 'Vue.js Playground',
+      subtitle: '',
     }
-  }
+  },
+
+  /* ------------------------------------------------------------------------ */
+
+  methods: {
+    addSubtitle: function() {
+      this.subtitle = 'Example of a subtitle';
+    }
+  },
 
   /* ------------------------------------------------------------------------ */
 }
@@ -26,6 +50,14 @@ export default {
 /* -------------------------------------------------------------------------- */
 h1 {
   color: #000;
+}
+
+/* -------------------------------------------------------------------------- */
+/* --- Classes -------------------------------------------------------------- */
+/* -------------------------------------------------------------------------- */
+.app-content {
+  text-align: center;
+  margin: 2rem;
 }
 
 /* -------------------------------------------------------------------------- */
